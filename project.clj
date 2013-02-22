a1562f27f92f022e7adec77f050886f68577d6e8
Matt House <matt@eightbitraptor.com>
2013-02-22 15:25:41 +0000

    Add midje to the project file

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject ninetynine-problems "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
+                      :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [midje "1.3.2-SNAPSHOT"]]
+            :dev-dependencies [[midje "1.3.2-SNAPSHOT"]
+                               [com.stuartsierra/lazytest "1.2.3"]]
+            :profiles {:dev {:plugins [[lein-midje "2.0.4"]]}}
+            :repositories {"stuart" "http://stuartsierra.com/maven2"})
