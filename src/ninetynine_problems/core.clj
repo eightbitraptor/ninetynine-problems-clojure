308f2626fcc0afe3a018f4fee2453c82cc7e2ebf
Matt House <matt@eightbitraptor.com>
2013-02-22 16:09:44 +0000

    98 is done

--- src/ninetynine_problems/core.clj
+++ src/ninetynine_problems/core.clj
@@ -4,3 +4,8 @@
   (if (empty? (rest args))
     (first args)
     (my-last (rest args))))
+
+(defn my-but-last [args]
+  (if (= 1 (count (rest args)))
+    args
+    (my-but-last (rest args))))
--- test/ninetynine_problems/core_test.clj
+++ test/ninetynine_problems/core_test.clj
@@ -2,5 +2,9 @@
   (:use midje.sweet)
   (:use ninetynine-problems.core))
 
-;; 99
+;; 99 - find the last element
 (fact (my-last '(:A :B :C :D)) => :D)
+
+;; 98 - find the penultimate element
+(fact (my-but-last '(:A :B :C :D)) => '(:C :D))
+
