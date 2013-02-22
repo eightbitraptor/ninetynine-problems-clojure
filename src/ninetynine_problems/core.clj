27da7c2264da3e7ec62d2308410dc01c495e8910
Matt House <matt@eightbitraptor.com>
2013-02-22 15:55:17 +0000

    And refactor to not cheat

--- src/ninetynine_problems/core.clj
+++ src/ninetynine_problems/core.clj
@@ -1,4 +1,6 @@
 (ns ninetynine-problems.core)
 
 (defn my-last [args]
-  (last args))
+  (if (empty? (rest args))
+    (first args)
+    (my-last (rest args))))
--- test/ninetynine_problems/core_test.clj
+++ test/ninetynine_problems/core_test.clj
@@ -2,4 +2,5 @@
   (:use midje.sweet)
   (:use ninetynine-problems.core))
 
+;; 99
 (fact (my-last '(:A :B :C :D)) => :D)
